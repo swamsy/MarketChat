cba15dcb1d322e6736a0e8107dd545e9a3df89da
Michael Swan <97557851+swamsy@users.noreply.github.com>
2023-08-07 21:52:43 -0400

    Update for heroku deployment

--- client/src/components/Loaders.js
+++ client/src/components/Loaders.js
@@ -2,7 +2,7 @@ import { BeatLoader } from 'react-spinners';
 import { PulseLoader } from 'react-spinners';
 
 export function CustomBeatLoader() {
-  return <BeatLoader color={'#3777c0'} loading={true} size={10} />;
+  return <BeatLoader color={'#14243d'} loading={true} size={10} />;
 }
 
 export function CustomPulseLoader () {
